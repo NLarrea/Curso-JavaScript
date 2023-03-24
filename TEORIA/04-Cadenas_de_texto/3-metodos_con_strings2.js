d130271af8beccf89e35b55529bb5531566ff820
nlarrea <larrea.naia@gmail.com>
2023-03-24 11:18:30 +0100

    cadenas_de_texto v2

--- TEORIA/04-Cadenas_de_texto/3-metodos_con_strings2.js
+++ TEORIA/04-Cadenas_de_texto/3-metodos_con_strings2.js
@@ -1,65 +1,94 @@
 // MÉTODOS CON STRINGS 2
 
 // CONVERTIR EN MAYÚSCULA / MINÚSCULA
+
 let input = "Escorpio";
 let db = "escorpio";
-console.log(db === input); // false
-console.log(db.toLowerCase() === input.toLowerCase()); // true
+
+console.log(db === input);                              // false
+console.log(db.toLowerCase() === input.toLowerCase());  // true
 // lo mismo con el .toUpperCase(), convirtiendo el texto a mayúsculas
 
-/* existe la opción de usar .toLocaleLowerCase() en vez de .toLowerCase(),
-pero realmente son lo mismo, solo que hay idiomas (turco, por ejemplo) en
-los que dan resultados diferentes. En este caso no nos es relevante */
+/* existe la opción de usar .toLocaleLowerCase() en vez de .toLowerCase(), pero
+realmente son lo mismo, solo que hay idiomas (turco, por ejemplo) en los que
+dan resultados diferentes. En este caso no nos es relevante */
+
 
 
 // CONCATENAR CADENAS
+
 // forma 1
-let str1 = "Hola soy la primera cadena. "; // con espacio al final
+let str1 = "Hola soy la primera cadena. ";      // con espacio al final
 let str2 = "Y yo soy la segunda cadena";
 console.log(str1.concat(str2));
 // Hola soy la primera cadena. Y yo soy la segunda
 
+
 // forma 2
-str1 = "Hola soy la primera cadena."; // sin espacio al final
+str1 = "Hola soy la primera cadena.";           // sin espacio al final
 console.log(str1.concat(" ", str2));
 // Hola soy la primera cadena. Y yo soy la segunda
+
 
 // forma 3
 console.log(str1 + " " + str2);
 // Hola soy la primera cadena. Y yo soy la segunda
-/* esta no es una buena práctica, ya que si se cambia el tipo de alguna de las variables
-str al tipo numérico (o las dos), podríamos estar sumando de forma numérica en lugar de
-estar concatenando */
+
+/* esta no es una buena práctica, ya que si se cambia el tipo de alguna de las
+variables str al tipo numérico (o las dos), podríamos estar sumando de forma
+numérica en lugar de estar concatenando */
+
 
 // forma 4
 console.log(`${str1} ${str2}`);
 // Hola soy la primera cadena. Y yo soy la segunda
 
 
+
 // QUITAR ESPACIOS AL PRINCIPIO Y FINAL
+
 let str_esp = "     Hola soy un string con espacios al inicio y al final           ";
-console.log(str_esp.length); // 51
+
+console.log(str_esp.length);                // 51
+
 // trim() -> quitamos espacios iniciales y finales
-console.log(str_esp.trim().length); // 40
+console.log(str_esp.trim().length);         // 40
+
 // trimStart() -> quitamos espacios iniciales
-console.log(str_esp.trimStart().length); // 63
+console.log(str_esp.trimStart().length);    // 63
+
 // trimEnd() -> quitamos espacios finales
-console.log(str_esp.trimEnd().length); // 57
+console.log(str_esp.trimEnd().length);      // 57
+
 
 
 // OBTENER CHAR DE DETERMINADA POSICIÓN
+
 // forma 1
 let str_pos = "Hola soy el string número 4";
-console.log(str_pos.charAt(5)); // s
+console.log(str_pos.charAt(5));         // s
+
+
 // forma 2
-console.log(str_pos[5]); // s
+console.log(str_pos[5]);                // s
+
 
 
 // OBTENER POSICIÓN DE PALABRA
+
 let str_palabra = "Hola soy Naia y me gusta el rugby. Mi nombre es Naia y mi apellido Larrea";
-console.log(str_palabra.indexOf("Naia")); // 9 -> devuelve la pos del primer char de esa palabra (la primera del string que coincida)
-console.log(str_palabra.charAt(9)); // N -> demostración
 
-console.log(str_palabra.indexOf("Nahia")); // -1 -> no lo encuentra
+/*
+.indexOf() -> devuelve la pos del primer char de esa palabra (la primera del
+string que coincida). Si no hay coincidencias, devuelve -1
+*/
+console.log(str_palabra.indexOf("Naia"));           // 9
+console.log(str_palabra.charAt(9));                 // N -> demostración
+console.log(str_palabra.indexOf("Nahia"));          // -1 -> no lo encuentra
 
-console.log(str_palabra.lastIndexOf("Naia")); // 48 -> devuelve la pos del primer char de la última palabra que coincida con esa
+/* 
+.lastIndexOf() -> devuelve la pos del primer char de la última palabra que
+coincida con esa. Si no hay coincidencias, devuelve -1 también
+*/
+console.log(str_palabra.lastIndexOf("Naia"));       // 48
+console.log(str_palabra.lastIndexOf("Nahia"));      // -1 -> no lo encuentra
--- TEORIA/04-Cadenas_de_texto/4-expresionesRegulares.js
+++ TEORIA/04-Cadenas_de_texto/4-expresionesRegulares.js
@@ -1,24 +1,39 @@
 // EXPRESIONES REGULARES
 
 // para aprender más sobre expresiones regulares: https://regexr.com
+// para practicar con regex: https://regex101.com/
+
+
 
 // CUÁNTAS VECES APARECE TEXTO
-let texto_largo = "Tito no es un mono cualquiera. A Tito no le gusta trepar por los árboles y odia comer plátanos. Él prefiere pasear por el bosque, oler las flores y recoger las nueces que se caen de los árboles."
-console.log(texto_largo.match(/no/g)); // [ 'no', 'no', 'no', 'no' ]
-// devuelve una lista con cuántas veces se repite ese trozo, puede ir dentro de una palabra
+
+let texto_largo = `Tito no es un mono cualquiera. A Tito no le gusta trepar\
+por los árboles y odia comer plátanos. Él prefiere pasear por el bosque, oler\
+las flores y recoger las nueces que se caen de los árboles.`
+
+console.log(texto_largo.match(/no/g));      // [ 'no', 'no', 'no', 'no' ]
+console.log(texto_largo.match(/qeu/g));     // null
+/*
+.match() -> devuelve una lista con cuántas veces se repite ese trozo, puede ir
+dentro de una palabra. Si no hay coincidencias, devuelve 'null'
+*/
+
 
 
 // SABER SI CONTIENE TEXTO / PALABRA
-console.log(texto_largo.includes("prefiere")); // true -> aparece
-console.log(texto_largo.includes("terremoto")); // false -> no aparece
+
+console.log(texto_largo.includes("prefiere"));      // true -> aparece
+console.log(texto_largo.includes("terremoto"));     // false -> no aparece
+
 
 
 // SABER SI EMPIEZA / TERMINA CON UNA PALABRA
-console.log(texto_largo.startsWith("Árboles")); // false -> no empieza por Árboles
-console.log(texto_largo.startsWith("Tito")); // true
-console.log(texto_largo.startsWith("tito")); // false -> es case sensitive
 
-console.log(texto_largo.endsWith("Tito")); // false
-console.log(texto_largo.endsWith("árboles.")); // true -> no olvidar el punto
-console.log(texto_largo.endsWith("árboles")); // false -> falta el punto
-console.log(texto_largo.endsWith("Árboles.")); // false -> es case sensitive
+console.log(texto_largo.startsWith("Árboles"));     // false
+console.log(texto_largo.startsWith("Tito"));        // true
+console.log(texto_largo.startsWith("tito"));        // false -> case sensitive
+
+console.log(texto_largo.endsWith("Tito"));          // false
+console.log(texto_largo.endsWith("árboles."));      // true -> acaba en punto
+console.log(texto_largo.endsWith("árboles"));       // false -> falta el punto
+console.log(texto_largo.endsWith("Árboles."));      // false -> case sensitive
