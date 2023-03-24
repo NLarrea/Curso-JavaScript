9020068a032f444827bf305ca0ccccb33e051677
nlarrea <larrea.naia@gmail.com>
2023-03-25 00:37:47 +0100

    depuracion_de_codigo v2

--- TEORIA/12-Depuracion_de_codigo/1-depuracionCodigo.js
+++ TEORIA/12-Depuracion_de_codigo/1-depuracionCodigo.js
@@ -1,35 +1,40 @@
 // DEPURACIÓN DE CÓDIGO
 
-/* consiste en ir ejecutando el código de forma continuada para comprobar que todas
-las partes del código funcionan */
+/* consiste en ir ejecutando el código de forma continuada para comprobar que
+todas las partes del código funcionan */
 
 const persona = {
     nombre: "Naia",
     edad: 24
 }
 
-console.log(persona); // punto de ruptura añadido
+console.log(persona);           // punto de ruptura añadido
 
-function obtenDobleEdad(edad){
+function obtenDobleEdad(edad) {
     return edad * 2;
 }
 
 const dobleEdad = obtenDobleEdad(persona.edad);
-console.log(dobleEdad); // 48 -> punto de ruptura
+console.log(dobleEdad);         // 48 -> punto de ruptura
 
-/* lo más cómodo para depurar el código es ir viendo por pantalla los valores de las variables,
-sin embargo, se puede ejecutar y depurar un código desde VSCode */
+/* lo más cómodo para depurar el código es ir viendo por pantalla los valores
+de las variables, sin embargo, se puede ejecutar y depurar un código desde
+VSCode */
 
 
 // OTRO EJEMPLO
-function obtenArray(edad){
+
+function obtenArray(edad) {
     let arrayNums = [];
-    for(let i=0; i<10; i++){
+
+    for (let i=0; i<10; i++) {
         const numero = edad + i;
         console.log(numero);
-        arrayNums = [...arrayNums, numero]; // para añadir numero a la lista
+        arrayNums = [...arrayNums, numero];     // añadir numero a la lista
     }
+    
     return arrayNums;
 }
+
 const array = obtenArray(persona.edad);
 console.log(array);
